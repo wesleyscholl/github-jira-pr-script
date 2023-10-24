4f4f2c8e3c5f82e9ab7fbbd1c57dde6df111ab9b
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2023-10-23 19:02:20 -1000

    Create vars.sh

---
+++ vars.sh
@@ -0,0 +1,5 @@
+alias pr='the path of the script/pull_request.sh'
+export jira_url=jira project base url
+export jira_access_token=email:Jiratoken
+export github_author=your github username
+export github_reviewers=reviewers github handles,seperated,by,comma
